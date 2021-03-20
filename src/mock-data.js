0e92da87d764a426d9c0b9c3a61fe931ebd2e30b
Nathan Chatwin <nathanchatwin@gmail.com>
2021-03-20 11:31:47 -0600

    fixed mock-data

--- src/mock-data.js
+++ src/mock-data.js
@@ -3,35 +3,35 @@ let mock = [{
     name: "To the Skies!",
     reward: "5 gold",
     location: "Kalimdor",
-    description: "Something good"
+    description: "Ride the hippogryph and bomb 12 catapults."
   },
   {
     id: 2,
     name: "Learning the Ropes",
     reward: "2 gold",
     location: "Eastern Kingdoms",
-    description: "Something good"
+    description: "Mount up and complete a practice joust."
   },
   {
     id: 3,
     name: "Dagger in the Dark",
     reward: "40 gold",
     location: "Kalimdor",
-    description: "Something good"
+    description: "Slay Reislek."
   },
   {
     id: 4,
     name: "Wild Things",
     reward: "12 gold",
     location: "Kalimdor",
-    description: "Something good"
+    description: "Find where they are."
   },
   {
     id: 5,
     name: "Onyx to Goodness",
     reward: "165 gold",
     location: "Northrend",
-    description: "Something good"
+    description: "Collect the onyx dragon's egg."
 
   },
   {
@@ -39,28 +39,28 @@ let mock = [{
     name: "A Second Hand",
     reward: "55 gold",
     location: "Eastern Kingdoms",
-    description: "Something good"
+    description: "Collect 8 troll hands."
   },
   {
     id: 7,
     name: "A Little Kelp from my Foes",
     reward: "82 gold",
     location: "Eastern Kingdoms",
-    description: "Something good"
+    description: "Collect kelp from vrykul spirits."
   },
   {
     id: 8,
     name: "Perserved in Amber",
     reward: "44 gold",
     location: "Northrend",
-    description: "Something good"
+    description: "Wake Korven the Prime."
   },
   {
     id: 9,
     name: "Last Piece of the Puzzle",
     reward: "4 gold",
     location: "Eastern Kingdoms",
-    description: "Something good"
+    description: "Solve the Lorewalker's puzzle."
 
   },
   {
@@ -68,77 +68,77 @@ let mock = [{
     name: "A Witness From History",
     reward: "2 gold",
     location: "Eastern Kingdoms",
-    description: "Something good"
+    description: "Witness the council meeting."
   },
   {
     id: 11,
     name: "Enemies Below",
     reward: "6 gold",
     location: "Kalimdor",
-    description: "Something good"
+    description: "Kill 8 kobolds."
   },
   {
     id: 12,
     name: "Psycho Mantid",
     reward: "1 gold",
     location: "Northrend",
-    description: "Something good"
+    description: "Slay 12 mantid foes."
   },
   {
     id: 13,
     name: "The Funky Monkey Brew",
     reward: "6 gold",
     location: "Eastern Kingdoms",
-    description: "Something good"
+    description: "Steal 8 hozen beers."
   },
   {
     id: 14,
     name: "The Kun-Lai Kicker",
     reward: "5 gold",
     location: "Pandaria",
-    description: "Something good"
+    description: "Kick the field goal to win the game."
   },
   {
     id: 15,
     name: "Capers",
     reward: "4 gold",
     location: "Kalimdor",
-    description: "Something good"
+    description: "Steal an ancient text, a rusty kettle, and a gold coin."
   },
   {
     id: 16,
     name: "The King and the Council",
     reward: "92 gold",
     location: "Pandaria",
-    description: "Something good"
+    description: "Defeat the Shadow Council."
   },
   {
     id: 17,
     name: "The Arena of Annihilation",
     reward: "47 gold",
     location: "Eastern Kingdoms",
-    description: "Something good"
+    description: "Become a champion in the Arena of Annihilation."
   },
   {
     id: 18,
     name: "Temple of the White Tiger",
     reward: "5 gold",
     location: "Northrend",
-    description: "Something good"
+    description: "Meet Chi-Ji at the Temple of the White Tiger."
   },
   {
     id: 19,
     name: "Rolo's Riddle",
     reward: "81 gold",
     location: "Kalimdor",
-    description: "Something good"
+    description: "Solve Rolo's Riddle."
   },
   {
     id: 20,
     name: "Replenshing the Pantry",
     reward: "8 gold",
     location: "Kalimdor",
-    description: "Something good"
+    description: "Return with 4 swift carrots, 4 hyrule herbs, and one hearty durian."
   }
 ]
 
